fc53cbddf7ffd419e8611ef16b462f3db22e9c6f
Matthew Forrest <mforrest00@gmail.com>
2021-08-22 18:16:20 -0500

    Refactoring for pytest fixtures

--- tests/data/test_google_trends.py
+++ tests/data/test_google_trends.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone
 import pytest
-from tests.fixtures.integration.google_trends import google_trends_gprop_news_search, google_trends_gprop_web_search
 from trader.data.base import EIGHT_MINUTE, ONE_DAY, ONE_MINUTE, ONE_MONTH
 from trader.data.google_trends import timeframe_base_label_to_date_ranges
 from trader.utilities.constants import GOOGLE_TRENDS_OTHER_SEARCH_BASE_DATE, GOOGLE_TRENDS_WEB_SEARCH_BASE_DATE
