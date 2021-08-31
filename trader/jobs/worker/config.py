62a65ee9e945ff22ea21de2ec29a9e87d0722c24
Matthew Forrest <mforrest00@gmail.com>
2021-08-31 01:31:39 -0500

    Removed RQ and setproctitle and working with celery instead

--- trader/jobs/worker/config.py
+++
@@ -1,7 +0,0 @@
-from trader.connections.queue import DATA_RETRIEVAL_QUEUE_NAME
-from trader.utilities.environment import REDIS_DATABASE, REDIS_HOST, REDIS_PORT
-
-
-REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}"
-
-QUEUES = [DATA_RETRIEVAL_QUEUE_NAME]
--- trader/tasks/__init__.py
+++ trader/tasks/__init__.py
@@ -2,6 +2,7 @@ from celery import Celery
 from celery.schedules import crontab
 from inflection import dasherize
 from trader.data.country import update_countries_from_iso
+from trader.data.cryptocurrency_exchange_rank import update_cryptocurrency_exchange_ranks_from_coin_market_cap
 from trader.data.cryptocurrency_rank import update_current_cryptocurrency_ranks_from_coin_market_cap
 from trader.data.standard_currency import update_standard_currencies_from_iso
 from trader.utilities.environment import CELERY_REDIS_DATABASE, CELERY_REDIS_HOST, CELERY_REDIS_PORT
@@ -13,6 +14,9 @@ app = Celery(__name__, broker=redis_endpoint, backend=redis_endpoint)
 
 update_countries_from_iso_task = app.task(update_countries_from_iso)
 update_standard_currencies_from_iso_task = app.task(update_standard_currencies_from_iso)
+update_cryptocurrency_exchange_ranks_from_coin_market_cap_task = app.task(
+    update_cryptocurrency_exchange_ranks_from_coin_market_cap
+)
 update_current_cryptocurrency_ranks_from_coin_market_cap_task = app.task(
     update_current_cryptocurrency_ranks_from_coin_market_cap
 )
@@ -25,6 +29,10 @@ app.conf.beat_schedule = {
     },
     dasherize(update_standard_currencies_from_iso_task.__name__): {
         "task": "trader.tasks.update_standard_currencies_from_iso_task",
+        "schedule": crontab(minute=0, hour=0, day_of_week="mon"),
+    },
+    dasherize(update_cryptocurrency_exchange_ranks_from_coin_market_cap_task.__name__): {
+        "task": "trader.tasks.update_cryptocurrency_exchange_ranks_from_coin_market_cap_task",
         "schedule": crontab(minute=0, hour=0, day_of_week="mon"),
     },
     dasherize(update_current_cryptocurrency_ranks_from_coin_market_cap_task.__name__): {
